b455e26f4d6dc2343ef926d2f7394bb87495cf07
khushboogithum <khushboochuarasiya19@gmail.com>
2025-01-05 23:21:53 +0530

    Your commit message

---
+++ application/views/admin/updater/index.php
@@ -0,0 +1,25 @@
+<style type="text/css">
+    .message-container {
+        text-align: center;
+        margin-top: 50px;
+        font-family: Arial, sans-serif;
+    }
+    .message-container h1 {
+        font-size: 24px;
+        color: #333;
+    }
+    .message-container a {
+        font-size: 18px;
+        color: #0066cc;
+        text-decoration: none;
+        font-weight: bold;
+    }
+    .message-container a:hover {
+        text-decoration: underline;
+    }
+</style>
+
+<div class="message-container">
+    <h1>Thank you for choosing I VIBE TECH</h1>
+    <a href="https://ivibetech.com" target="_blank">Visit ivibetech.com</a>
+</div>
