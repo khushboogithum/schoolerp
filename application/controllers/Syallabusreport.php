9208da206ed4a8b6dd8d29f9547fec4d42679aca
Sonugit1008 <sonukumarg337@gmail.com>
2025-02-05 00:17:16 +0530

    sylbus report classwise complate

--- application/controllers/Syallabusreport.php
+++ application/controllers/Syallabusreport.php
@@ -11,9 +11,7 @@ class Syallabusreport extends Admin_Controller
     {
         parent::__construct();
         $this->load->library('form_validation');
-        $this->load->model('Syallabusreport_model');  
-
-
+        $this->load->model('Syallabusreport_model');
     }
 
     public function index()
@@ -21,37 +19,75 @@ class Syallabusreport extends Admin_Controller
         if (!$this->rbac->hasPrivilege('syallabusreport', 'can_view')) {
             access_denied();
         }
-        $classlist         = $this->class_model->get();
-        $data['classlist'] = $classlist;
-
-
-        
-        $syallabusReport = $this->Syallabusreport_model->syallabusReport();
-        $data['subjectgroup'] = $this->Syallabusreport_model->getGroupByClassandSection();
-        $workData = [];
-        foreach ($syallabusReport as $work) {
-            $lesson_number = $work['lesson_number'];
-            if ($work['total_lessons'] > 0) {
-                $work['syllabus_percentage'] = round(($lesson_number / $work['total_lessons']) * 100, 2);
-            } else {
-                $work['syllabus_percentage'] = 0;
-            }
-
-            $workData[] = $work;
-        }
-        $data['syallabusReport']=$workData;
-        
-
         $this->session->set_userdata('top_menu', 'syallabusreport');
         $this->session->set_userdata('sub_menu', 'syallabusreport/index');
         $data['title']      = 'Syallabus Report';
         $data['title_list'] = 'Syallabus Report';
-       
-       
-        $this->load->view('layout/header', $data);
-        $this->load->view('syallabusreport/syallabus_report', $data);
-        $this->load->view('layout/footer', $data);
-    }
-    
 
+        $classlist         = $this->class_model->get();
+        $data['classlist'] = $classlist;
+        $data['subjectgroup'] = $this->Syallabusreport_model->getGroupByClassandSection();
+
+
+
+        $workData = [];
+
+        ///////////////////////////////filter wise data/////////////////////////////////////////////////////////
+
+        $data['report_type'] = $report_type = $this->input->post('report_type');
+
+        $this->form_validation->set_rules('report_type', $this->lang->line('report_type'), 'required');
+        $this->form_validation->set_message('required', 'Please select atleast one.');
+        $this->form_validation->set_rules('from_date', $this->lang->line('from_date'), 'required');
+        $this->form_validation->set_rules('to_date', $this->lang->line('to_date'), 'required');
+        if ($report_type == 'class_wise') {
+
+            $this->form_validation->set_rules('class_id', $this->lang->line('class_id'), 'required');
+            $this->form_validation->set_rules('section_id', $this->lang->line('section_id'), 'required');
+        }
+        if ($report_type == 'teacher_wise') {
+            $this->form_validation->set_rules('teacher_id', $this->lang->line('teacher_id'), 'required');
+        }
+        if ($report_type == 'subject_wise') {
+            $this->form_validation->set_rules('subject_group_id', $this->lang->line('subject_group_id'), 'required');
+            $this->form_validation->set_rules('subject_id', $this->lang->line('subject_id'), 'required');
+        }
+
+        $data['from_date'] = $from_date = $this->input->post('from_date');
+        $data['to_date'] = $to_date = $this->input->post('to_date');
+
+
+
+
+        if ($this->form_validation->run() == false) {
+            $data['syallabusReport'] = $workData;
+            $this->load->view('layout/header', $data);
+            $this->load->view('syallabusreport/syallabus_report', $data);
+            $this->load->view('layout/footer', $data);
+        } else {
+            if ($report_type == 'class_wise') {
+
+
+                $data['class_id'] = $class_id = $this->input->post('class_id');
+                $data['section_id'] = $section_id = $this->input->post('section_id');
+
+                $syallabusReport = $this->Syallabusreport_model->syallabusReport($from_date,$to_date,$class_id,$section_id);
+                foreach ($syallabusReport as $work) {
+                    $lesson_number = $work['lesson_number'];
+                    if ($work['total_lessons'] > 0) {
+                        $work['syllabus_percentage'] = round(($lesson_number / $work['total_lessons']) * 100, 2);
+                    } else {
+                        $work['syllabus_percentage'] = 0;
+                    }
+                    $workData[] = $work;
+                }
+            }
+
+
+            $data['syallabusReport'] = $workData;
+            $this->load->view('layout/header', $data);
+            $this->load->view('syallabusreport/syallabus_report', $data);
+            $this->load->view('layout/footer', $data);
+        }
+    }
 }
